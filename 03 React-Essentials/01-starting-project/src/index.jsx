1da1e5dd7ff8ed0e57cda38b69783c03f29a9d48
Kristin <Kristineliseboyd13@gmail.com>
2024-04-25 17:19:51 -0700

    organizes imports

--- 03 React-Essentials/01-starting-project/src/index.jsx
+++ 03 React-Essentials/01-starting-project/src/index.jsx
@@ -1,7 +1,11 @@
+// Libraries
 import ReactDOM from "react-dom/client";
-
+// Components
 import App from "./App.jsx";
+// Styling
 import "./index.css";
 
+
 const entryPoint = document.getElementById("root");
+
 ReactDOM.createRoot(entryPoint).render(<App />);
