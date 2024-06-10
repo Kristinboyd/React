e25cbf872a9b4359cba3385e3e0a0e0d85ca4241
Kristin <Kristineliseboyd13@gmail.com>
2024-06-10 14:33:04 -0700

    cleans up code

--- 04 Essentials Deep Dive/01-starting-project/src/App.jsx
+++ 04 Essentials Deep Dive/01-starting-project/src/App.jsx
@@ -3,7 +3,6 @@ import CoreConcepts from './components/CoreConcepts.jsx';
 import Examples from './components/Examples.jsx';
 import Header from './components/Header/Header.jsx';
 
-
 export default function App() {
   return (
     <div>
