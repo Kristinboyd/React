4490429d81f1373027b3a595c01d4298866bca38
Kristin <Kristineliseboyd13@gmail.com>
2024-06-20 09:37:50 -0700

    formats file

--- 04 Essentials Deep Dive/01-starting-project/src/index.jsx
+++ 04 Essentials Deep Dive/01-starting-project/src/index.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-import App from "./App.jsx";
-import "./index.css";
-
-const entryPoint = document.getElementById("root");
+const entryPoint = document.getElementById('root');
 ReactDOM.createRoot(entryPoint).render(<App />);
